49c30803517efb6e1f9da5105fdd58940c9a5260
Tanisha780 <141040330+Tanisha780@users.noreply.github.com>
2024-02-11 13:12:53 +0000

    changes

---
+++ basics/conversion.js
@@ -0,0 +1,10 @@
+let score =44
+console.log(typeof score);
+let number = 33
+let string = String(number)
+console.log(string);
+console.log(typeof string);
+let someString = "tanisha"
+let someNumber = Number(someString)
+console.log(parseInt(someNumber,10));
+console.log(typeof someNumber);
