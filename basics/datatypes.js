fdf9d378180396b265ae12f68de9e4ee4d487941
Tanisha780 <141040330+Tanisha780@users.noreply.github.com>
2024-02-11 09:56:52 +0000

    changes

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all js code as newer version
+//alert() we are using node js not browser
+console.log("tanisha")
+let name = "tanisha"
+let age = 20
+let isLoggedIn = false
+// number ->  2 to power 53
+//bigint
+//string->""
+//boolean->true/false
+//null-> standlone value
+// undefined => not defined
+//symbol->unique
+// object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
